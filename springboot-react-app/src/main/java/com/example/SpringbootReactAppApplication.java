6e165efc85750f2e1f262e953201f7e15f3a45e6
Douglas Lima <douglas.00000851194@unicap.br>
2025-03-17 22:07:21 +0000

    Adicionando projeto Spring Boot com React

---
+++ springboot-react-app/src/main/java/com/example/SpringbootReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactAppApplication.class, args);
+	}
+
+}
